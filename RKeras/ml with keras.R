87ff1e22d339f1c5dd5e4e195041e6b5c9594cfa
Anton <agavrailov@gmail.com>
2022-02-03 00:29:54 +0200

    Subset a data frame to test on it.

--- RKeras/ml with keras.R
+++ RKeras/ml with keras.R
@@ -9,8 +9,6 @@ neural.train = function(model,XY)
   Model %>%
     
     layer_dense(units=2,activation='relu',input_shape = c(ncol(X))) %>%
-    layer_lstm(units = 2) %>%
-    layer_lstm(units = 2) %>%
     layer_dropout(rate = 0.2) %>%
     layer_dense(units = 1, activation = 'sigmoid')
   
@@ -21,7 +19,7 @@ neural.train = function(model,XY)
   
   Model %>% fit(X, Y,
                 epochs = 20, batch_size = 20,
-                validation_split = 0, shuffle = FALSE)
+                validation_split = 0.3, shuffle = FALSE)
   
   Models[[model]] <<- Model
 }
@@ -54,4 +52,8 @@ neural.init = function()
   Models <<- vector("list")
 }
 
-neural.train("А",as.matrix(seq(1:100),seq(1:1000)))
+#Prepare the table input table and fields
+Models<-NULL
+input_fields<-c("Open","Label1")
+df_t<-df[input_fields]
+neural.train(1, df_t)
