1ca031756936ecf228ade1394d27e2897a6227a1
Anton <agavrailov@gmail.com>
2022-02-09 22:18:50 +0200

    Generator working

--- RKeras/generator.R
+++ RKeras/generator.R
@@ -1,17 +1,31 @@
 library(Keras)
+tsteps = 5  #window size
+lag = 10  #Labels number of rows ahead
+batch_size = 20
+epochs = 20
+
 XY <<- read.csv("D:\\My Documents\\R\\ml\\data\\training_data.csv",header = TRUE)
 XY <<- XY[c("Time","Open.1","High.1", "Label1")]
-X <- XY[2]
-X <- XY[,-ncol(XY)]
-Y <- as.matrix(XY[,ncol(XY)])
-tsteps = 10
+X <- as.matrix(XY[2])
+# X <- as.matrix(seq(1:20))   #used to test the lagging and timeseries_generator
+Y <-rbind(matrix(rep(mean(X),lag)),head(X,-lag))   #lagged version of trainingg data
 
-generator = timeseries_generator(X,X, 
+# X <- XY[,-ncol(XY)]
+# Y <- as.matrix(XY[,ncol(XY)])
+
+generator = timeseries_generator(X,Y, 
                                  length = tsteps, 
-                                 batch_size = 100, 
+                                 batch_size = batch_size, 
                                  start_index = 1, 
-                                 end_index = nrow(XY),
+                                 end_index = nrow(XY)-batch_size,
+                                 sampling_rate = 1,
+                                 stride = 3,
                                  shuffle = FALSE)
+# length(generator)
+# for(i in seq(1:length(generator))){
+#   x = y = generator[i]
+#   print(x)
+# }
 
 Model <- keras_model_sequential() 
 Model %>%
@@ -30,11 +44,12 @@ Model %>% compile(
   optimizer = 'rmsprop', 
   metrics = c('accuracy'))
 
-
-for(i in seq(1:length(generator))){
-  x = generator[i]
-  print(c(x, y))
-}
-
-Model %>% fit(generator,
+Model %>% fit(generator, 
+              batch_size = 10,
               epochs = epochs)
+
+# TODO make a one step prediction out of sample
+# x_input = array([9, 10]).reshape((1, n_input, n_features))
+# yhat = model.predict(x_input, verbose=0)
+# print(yhat)
+# https://machinelearningmastery.com/how-to-use-the-timeseriesgenerator-for-time-series-forecasting-in-keras/
