bbc4089e96b2fa8d9019ee59542cdd9dca6ccd68
Anton <agavrailov@gmail.com>
2022-02-09 16:11:10 +0200

    stash merge

--- RKeras/generator.R
+++ RKeras/generator.R
@@ -44,4 +44,3 @@ Model %>% fit(generator,
 # yhat = model.predict(x_input, verbose=0)
 # print(yhat)
 # https://machinelearningmastery.com/how-to-use-the-timeseriesgenerator-for-time-series-forecasting-in-keras/
-# https://machinelearningmastery.com/how-to-use-the-timeseriesgenerator-for-time-series-forecasting-in-keras/
