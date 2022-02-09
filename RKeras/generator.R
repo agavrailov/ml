0b957e43341925e269bde6c7ff154f275f8b20cd
Anton <agavrailov@gmail.com>
2022-02-09 23:25:32 +0200

    Minimalistic size of NN- better results

--- RKeras/generator.R
+++ RKeras/generator.R
@@ -1,14 +1,17 @@
-library(Keras)
+# library(Keras)
 tsteps = 5  #window size
 lag = 10  #Labels number of rows ahead
 batch_size = 20
 epochs = 20
 
-XY <<- read.csv("D:\\My Documents\\R\\ml\\data\\training_data.csv",header = TRUE)
-XY <<- XY[c("Time","Open.1","High.1", "Label1")]
-X <- as.matrix(XY[2])
-# X <- as.matrix(seq(1:20))   #used to test the lagging and timeseries_generator
-Y <-rbind(matrix(rep(mean(X),lag)),head(X,-lag))   #lagged version of trainingg data
+XY <- read.csv("D:\\My Documents\\R\\ml\\data\\training_1c.csv",header = TRUE)
+# write.csv(XY[2], file = "D:\\My Documents\\R\\ml\\data\\training_1c.csv")
+
+# XY <- XY[c("Time","Open.1","High.1", "Label1")]
+# XY <- head(XY,
+           # trunc( nrow(XY)/batch_size) *  batch_size)
+X <- as.matrix(XY["Open.1"])
+Y <-rbind(matrix(rep(mean(X),lag)),head(X,-lag))   #lagged version of training data
 
 # X <- XY[,-ncol(XY)]
 # Y <- as.matrix(XY[,ncol(XY)])
@@ -29,13 +32,13 @@ generator = timeseries_generator(X,Y,
 
 Model <- keras_model_sequential() 
 Model %>%
-  layer_lstm(units = 50, 
+  layer_lstm(units = 5, 
              input_shape = c(tsteps, ncol(X)),
              batch_size = batch_size,
              return_sequences = TRUE, 
              stateful = TRUE) %>% 
-  layer_dropout(rate = 0.2) %>%
-  layer_lstm(units = 50,
+  layer_dropout(rate = 0.0) %>%
+  layer_lstm(units = 5,
              return_sequences = FALSE, 
              stateful = TRUE) %>% 
   layer_dense(units = 1)
@@ -45,8 +48,9 @@ Model %>% compile(
   metrics = c('accuracy'))
 
 Model %>% fit(generator, 
-              batch_size = 10,
-              epochs = epochs)
+              batch_size = batch_size,
+              epochs = epochs,
+              ) 
 
 # TODO make a one step prediction out of sample
 # x_input = array([9, 10]).reshape((1, n_input, n_features))
