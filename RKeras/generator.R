a7867761c63bfd6f8069b09329d7b1d54d48ffb9
Anton <agavrailov@gmail.com>
2022-02-15 17:42:11 +0200

    rows_ahead 5, val_loss: 0.07

--- RKeras/generator.R
+++ RKeras/generator.R
@@ -1,7 +1,7 @@
 library(keras)
 tsteps = 3  #window size
 rows_ahead = 5  #prediction Labels are n rows ahead of the current
-batch_size = 400
+batch_size = 800
 epochs = 40
 split = 0.7   #part of data used for training 
 LSTM_units = 50
