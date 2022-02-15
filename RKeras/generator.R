5685f9cf38d981474e5991fd08aae1910857f742
Anton <agavrailov@gmail.com>
2022-02-15 15:54:25 +0200

    Validation works

--- RKeras/generator.R
+++ RKeras/generator.R
@@ -8,23 +8,24 @@ split = 0.7   #part of data used for training
 XY <- read.csv("D:\\My Documents\\R\\ml\\data\\training_data.csv",header = TRUE)
 # XY<-as.data.frame(1:200)
 
-## Split training validation and test sets
-# XY.tr
+# XY.tr training set
 XY.tr <- head(XY,nrow(XY)*split)
 extra_rows <- (nrow(XY.tr)-tsteps-1) %% (batch_size)  #LSTM cells need input to be divisible by batch_size AFTER split
 extra_rows
 if (extra_rows) XY.tr<-head(XY.tr,-extra_rows)
 
-# XY.val
+# XY.val validation set
 XY.val <-tail(XY, -split*nrow(XY))
-data.frame(nrow(XY), nrow(XY.tr), nrow(XY.val))
-X <- as.matrix(XY.tr[ncol(XY.tr)])
+extra_rows <- (nrow(XY.val)-tsteps-1) %% (batch_size)  #LSTM cells need input to be divisible by batch_size AFTER split
+extra_rows
+if (extra_rows) XY.val<-head(XY.val,-extra_rows)
 
-#Create lagged version of training data
-Y <-rbind(matrix(rep(mean(X)),rows_ahead),head(X,-rows_ahead))  
-# rownames(Y) <- NULL
+data.frame(nrow(XY), nrow(XY.tr), nrow(XY.val))
 
 #the generator
+# X <- as.matrix(XY.tr[ncol(XY.tr)])
+X <- as.matrix(XY.tr["Open.1"])
+Y <-rbind(matrix(rep(mean(X)),rows_ahead),head(X,-rows_ahead))  #Create lagged version of training data
 generator = timeseries_generator(X,Y, 
                                  length = tsteps, 
                                  batch_size = batch_size, 
@@ -34,7 +35,20 @@ generator = timeseries_generator(X,Y,
                                  stride = 1,
                                  shuffle = FALSE)
 
-# for(i in seq(1:length(generator))){
+#the generator for validation
+X.val <- as.matrix(XY.val[ncol(XY.val)])
+X.val <- as.matrix(XY.val["Open.1"])
+Y.val <-rbind(matrix(rep(mean(X.val)),rows_ahead),head(X.val,-rows_ahead))  #Create lagged version of training data
+generator.val = timeseries_generator(X.val,
+                                 Y.val,
+                                 length = tsteps, 
+                                 batch_size = batch_size, 
+                                 start_index = 1, 
+                                 end_index = nrow(X.val)-1,
+                                 sampling_rate = 1,
+                                 stride = 1,
+                                 shuffle = FALSE)
+# for(i in seq(1:length(generator.val))){
 #   x = y = generator[i]
 #   print(x)
 # }
@@ -61,12 +75,10 @@ Model %>% compile(
 Model %>% fit(generator, 
               batch_size = batch_size,
               epochs = epochs,
-
+              validation_data = generator.val
               ) 
 
-# TODO fix batchsize. works only with  20
-# make a one step prediction out of sample
+# TODO 
 # x_input = array([9, 10]).reshape((1, n_input, n_features))
 # yhat = model.predict(x_input, verbose=0)
-# print(yhat)
 # https://machinelearningmastery.com/how-to-use-the-timeseriesgenerator-for-time-series-forecasting-in-keras/
