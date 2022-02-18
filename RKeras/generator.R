e2e2bb3bddc027e659127437b1f310c7f49b8be8
Anton <agavrailov@gmail.com>
2022-02-18 19:37:40 +0200

    Multivariate working. Not accurate - val_loss 6.5

--- RKeras/generator.R
+++ RKeras/generator.R
@@ -7,7 +7,7 @@ split = 0.7   #part of data used for training
 LSTM_units = 50
 
 XY <- read.csv("D:\\My Documents\\R\\ml\\data\\training_data.csv",header = TRUE)
-XY <- XY[c("Open.1","Label1")]  #add as many columns as we need
+XY <- XY[c("Open.1","High.1","Label1")]  #add as many columns as we need
 
 # XY.tr training set
 XY.tr <- head(XY,nrow(XY)*split)
@@ -21,8 +21,8 @@ if (extra_rows) XY.val<-head(XY.val,-extra_rows)
 
 #the generator
 X <- as.matrix(XY.tr[,-ncol(XY.tr)])  #all, but last column
-# Y <- as.matrix(XY.tr[, ncol(XY.tr)])  #last column
-Y <-rbind(matrix(rep(mean(X[1:rows_ahead])),rows_ahead),head(X,-rows_ahead))  #Create lagged version of training data
+Y <- as.matrix(XY.tr[, ncol(XY.tr)])  #last column
+Y <- rbind(as.matrix(rep(mean(Y[1:rows_ahead,]), rows_ahead)),head(Y,-rows_ahead)) #Create lagged version of last column
 generator = timeseries_generator(X,Y, 
                                  length = tsteps, 
                                  batch_size = batch_size, 
@@ -34,8 +34,9 @@ generator = timeseries_generator(X,Y,
 
 #the generator for validation
 X.val <- as.matrix(XY.val[,-ncol(XY.val)])  #all, but last column
-# Y.val <- as.matrix(XY.val[, ncol(XY.val)])  #last column
-Y.val <-rbind(matrix(rep(mean(X.val)),rows_ahead),head(X.val,-rows_ahead))  #Create lagged version of training data
+Y.val <- as.matrix(XY.val[, ncol(XY.val)])  #last column
+Y.val <- rbind(as.matrix(rep(mean(Y.val[1:rows_ahead,]), rows_ahead)),head(Y.val,-rows_ahead)) #Create lagged version of last column
+
 generator.val = timeseries_generator(X.val,
                                  Y.val,
                                  length = tsteps, 
@@ -45,6 +46,7 @@ generator.val = timeseries_generator(X.val,
                                  sampling_rate = 1,
                                  stride = 1,
                                  shuffle = FALSE)
+
 # for(i in seq(1:length(generator.val))){
 #   x = y = generator[i]
 #   print(x)
@@ -59,6 +61,7 @@ Model %>%
              return_sequences = TRUE, 
              stateful = TRUE,
              activation = 'tanh') %>% 
+  
   layer_lstm(units = LSTM_units,
              return_sequences = FALSE, 
              stateful = TRUE,
@@ -68,8 +71,7 @@ Model %>%
 Model %>% compile(
   loss = 'mse', 
   optimizer = optimizer_adam(
-    learning_rate = 0.001, 
-    decay = 0.0001), 
+    learning_rate = 0.003), 
   metrics = c('accuracy'),
   )
 
@@ -80,6 +82,10 @@ Model %>% fit(generator,
               ) 
 
 # TODO 
+# reshape input to be [samples, time steps, features]
+# X_train = np.reshape(X_train, (X_train.shape[0], 1, X_train.shape[1]))
+
+
 # x_input = array([9, 10]).reshape((1, n_input, n_features))
 # yhat = model.predict(x_input, verbose=0)
 # https://machinelearningmastery.com/how-to-use-the-timeseriesgenerator-for-time-series-forecasting-in-keras/
